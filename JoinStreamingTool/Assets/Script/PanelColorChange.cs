c657f473882c974b042ae47975e64b83cdf65f83
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-02-07 17:33:58 +0900

    ID入力を可変
    ×ボタンで非表示にできる

---
+++ JoinStreamingTool/Assets/Script/PanelColorChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelColorChange : MonoBehaviour
+{
+    public void Show()
+    {
+        GetComponent<Image>().color = new Color32 (255, 0, 0, 255);
+    }
+
+    public void Hide()
+    {
+        GetComponent<Image>().color = new Color32 (255, 0, 0, 0);
+    }
+}
---
+++ JoinStreamingTool/Assets/Script/VisualizationSwitchingObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizationSwitchingObject : MonoBehaviour
+{
+    public GameObject VisualizationObject;
+    
+    public void Show()
+    {
+        VisualizationObject.SetActive (true);
+    }
+    public void Hide()
+    {
+        VisualizationObject.SetActive (false);
+    }
+}
