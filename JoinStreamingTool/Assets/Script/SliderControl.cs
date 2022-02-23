1e11f20fc233ed2e6f10c7f0250e2cef44c3053d
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-02-23 16:25:18 +0900

    機能追加
    ・セクションを順番に実行
    ・設定機能

---
+++ JoinStreamingTool/Assets/Script/SliderControl.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour
+{
+    private Slider SliderObject;
+    private float TotalTime;
+    public bool UpdateActive = false;
+    private bool TimerFinished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SliderObject = GetComponent<Slider>();
+
+        SliderObject.value = SliderObject.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!UpdateActive){return;}
+
+        TimerValueCheck();
+        SubtractionTimer();
+    }
+
+    // タイマーの最大値設定
+    public void TimerGaugeMaxValueChange(int Value)
+    {
+        SliderObject.maxValue = Value;
+    }
+
+    public void TimerActive()
+    {
+        UpdateActive = true;
+    }
+
+    // タイマーの終了フラグ取得
+    public bool GetTimerFinished()
+    {
+        return TimerFinished;
+    }
+
+    // タイマー設定の初期化
+    public void InitializationTimer()
+    {
+        SliderObject.value = SliderObject.maxValue;
+        UpdateActive = false;
+        TimerFinished = false;
+    }
+
+    // タイマーの数値チェック
+    private void TimerValueCheck()
+    {
+        // 0以上
+        if(SliderObject.value > 0){return;}
+
+        // 0以下
+        SliderObject.value = 0;
+        UpdateActive = false;
+        TimerFinished = true;
+    }
+
+    // タイマーの減算処理
+    private void SubtractionTimer()
+    {
+        TotalTime -= Time.deltaTime;
+
+        SliderObject.value = SliderObject.maxValue + TotalTime;
+
+        /* 0以下の処理
+        if(SliderObject.value <= 0)
+        {
+            //this.transform.SetAsFirstSibling();
+            //localScaleChange(1.0f);
+        }*/
+    }
+}
