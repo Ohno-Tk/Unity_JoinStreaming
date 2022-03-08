e30f930ae4a1cb649b44f43ef652a37f238406ff
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-03-09 07:55:04 +0900

    [add]キー入力を追加
    issues #7 キー入力を追加
    矢印上を押すと募集参加させるようにしました。

---
+++ JoinStreamingTool/Assets/Script/PlayerInputKey.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// プレイヤーに入力をさせる
+public class PlayerInputKey : MonoBehaviour
+{
+    public KeyCode StartSectionKey = KeyCode.UpArrow;
+    public KeyCode SettingKey = KeyCode.Escape;
+    public GameObject StartSection;
+    public GameObject Setting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 募集開始
+        if (Input.GetKey (StartSectionKey))
+        {
+            Debug.Log("募集を開始します");
+            StartSection.GetComponent<Observer>().StartTimer();
+        }
+        else if(Input.GetKey (SettingKey))
+        {
+            Debug.Log("設定を立ち上げます");
+            Setting.SetActive (true);
+        }
+    }
+}
