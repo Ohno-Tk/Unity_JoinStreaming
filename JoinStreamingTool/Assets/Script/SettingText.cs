7c546ea487fc87d1bfee57100bce9bbf3c29d57a
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-03-16 16:39:57 +0900

    [add]ゲームID入力機能追加
    ゲームID入力して
    画面上に表示できるようにしました

---
+++ JoinStreamingTool/Assets/Script/SettingText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingText : MonoBehaviour
+{
+
+    public InputField SettingInputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Text> ().text = SettingInputField.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
